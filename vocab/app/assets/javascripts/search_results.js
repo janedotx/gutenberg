65a1539eef9d29f837865d90a44aea745c799614
jian <jane.x.huang@gmail.com>
2013-06-24 11:45:17 -0700

    frequency banding

---
+++ vocab/app/assets/javascripts/search_results.js
@@ -0,0 +1,17 @@
+/* Place all the behaviors and hooks related to the matching controller here.
+ All this logic will automatically be available in application.js.
+ You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+ */
+
+$("document").ready(function() {
+  var checkboxes = $("input");
+  for (var i = 0; i < checkboxes.size(); i++) {
+    console.log("in for loop");
+    console.log($(checkboxes[i]).attr("id"));
+    var curbox = $(checkboxes[i]);
+    var f = function (i) { console.log(i) };
+    curbox.click(f)//(function(i) { 
+//      return function() {console.log(i); }
+//      }));
+  }
+})
